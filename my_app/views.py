cf2b0184eadef4b56b2202481d3a8ed08cdcf9b2
ruizr15 <71466308+ruizr15@users.noreply.github.com>
2020-09-19 19:40:26 -0400

    added buttons

--- my_app/views.py
+++ my_app/views.py
@@ -3,9 +3,13 @@ from flask import render_template, request, redirect
 import requests
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("/home/landing.html")
 
-@app.route("/about")
-def about():
+@app.route("/teacher")
+def teacher():
     return render_template("/teacher/dashboard.html")
+
+@app.route("/student")
+def student():
+    return render_template("/student/dashboard.html")
