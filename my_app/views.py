46b9ee646a209f039fd8b71a8409bf99206dc0d8
ruizr15 <71466308+ruizr15@users.noreply.github.com>
2020-09-20 00:32:07 -0400

    made buttons work

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,16 @@
 from my_app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for, flash
 import requests
 
-@app.route("/")
+app.secret_key = '3'
+
+@app.route("/", methods=['GET','POST'])
 def home():
+    if request.method == "POST":
+        if request.form["login"] == "teacher":
+            return redirect(url_for("teacher"))
+        if request.form["login"] == "student":
+            return redirect(url_for("student"))
     return render_template("/home/landing.html")
 
 @app.route("/teacher")
